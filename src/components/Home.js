57847d4d9448dd668459e705e3d59b380929ce81
Nicole Borabo <borabonicole19@gmail.com>
2025-05-02 17:45:19 +0800

    centralized question mark icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,9 @@ function Home({ onStart }) {
   return (
     <div className="quiz-container">
       <Card className="p-5 shadow-lg text-center">
-        <FaQuestionCircle size={80} className="text-success mb-4" />
+      <div className="d-flex justify-content-center mb-4">
+        <FaQuestionCircle size={80} className="text-success" />
+        </div>
         <h2 className="mb-4">Will you take the quiz now?</h2>
         <Button variant="success" size="lg" onClick={onStart}>
           Start Now
